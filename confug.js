47956f76d57c20f7573fbdbc2c99ffa2abfa6ad1
MrBeginerQD <mrdadeh@gmail.com>
2025-11-30 21:07:39 +0330

    Create confug.js

---
+++ confug.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    // لینک کانال تلگرام شما
+    telegramChannelUrl: "https://t.me/DataFlora",
+    
+    // عنوان سایت
+    siteTitle: "DataFlora",
+    
+    // منابع پروکسی
+    sources: {
+        primary: "https://raw.githubusercontent.com/hookzof/socks5_list/master/tg/mtproto.json",
+        backup: "https://raw.githubusercontent.com/Firmfox/Proxify/refs/heads/main/telegram_proxies/mtproto.txt"
+    },
+
+    // زمان کش شدن پروکسی‌ها به دقیقه (برای سرعت بالا)
+    cacheTimeMinutes: 10
+};
